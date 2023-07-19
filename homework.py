6e70941d98fe0917b6ceea90684dff5c8bded264
Tornikekarelidz <138922419+Tornikekarelidz@users.noreply.github.com>
2023-07-19 23:30:03 +0400

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,7 @@
+exlandeli_dro = 2023
+your_age = int(input("ramdeni wlis xar?: "))
+time = int(input("ramdeni wlit imogzaurebdi momavalshi?: "))
+future = exlandeli_dro + time
+future_age = your_age + time
+print("shen gadaxval " + str(future) + " " + "wels" + " " + "da shen iknebi" + " " + 
+str(future_age) + " " + "wlis")
